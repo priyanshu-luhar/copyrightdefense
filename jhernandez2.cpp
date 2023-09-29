6191521a43519f0f1cef7efa994f147be32c8af4
JosephHernandez1324 <143636261+JosephHernandez1324@users.noreply.github.com>
2023-09-29 09:44:42 -0700

    Add files via upload
    Uploaded individual file for Joseph Hernandez

---
+++ jhernandez2.cpp
@@ -0,0 +1,14 @@
+/* Features of the game */
+
+// Start of game menu
+// Game Over Screen
+// Potential skin for main guy, no character select
+// Potential Original Character as the main guy
+// Collectible buffs on the floor 
+// More than one map
+// Multiple sprites and sprite sheets used
+// Keyboard input, no mouse input
+// Collision between the player character and the enemies
+// Multiple animations implemented (Attack, hit, etc.)
+// Dynamic Health bar
+// Dynamic statuses that affect the player and enemies
