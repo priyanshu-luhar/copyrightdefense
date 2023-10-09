17fe5f2d64c6536f24d016b3e49ea9fc8e77e2b0
CarlosSandoval360 <113050074+CarlosSandoval360@users.noreply.github.com>
2023-10-09 08:29:28 -0700

    Add files via upload
    Moved the Global and x11 Classes into the header file so that the cpp can execute correctly.
    
    Added the modified menu code into the cpp file so that it is called in main as a function instead of being written and executed in the main function

--- csandoval.cpp
+++ csandoval.cpp
@@ -1,22 +1,84 @@
 //this is carlos's individual source file.
 
-// Start and Pause Menu
-// Potential Original Character
-// Game over screen
-// Potential Skin Select for maps and character
-// Character Buffs
-
-//Map Ideas: 
-
-
-// Player Colosion
-// Asteroid to Player Agression
-
-#include "fonts.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <ctime>
+#include <cmath>
+#include <X11/Xlib.h>
+#include <X11/keysym.h>
 #include <GL/glx.h>
+#include "log.h"
+#include "fonts.h"
+#include "csandoval.h"
+
+extern Global gl;
+extern X11_wrapper x11;
 
 
-	
+void handleMenu() {
+    int menuChoice = 0;
+    bool inMenu = true;
+
+    while (inMenu) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glPushMatrix();
+        glColor3f(1.0, 1.0, 1.0);
+        Rect r;
+        r.center = 0;
+        r.bot = gl.yres - 20;
+        r.left = 10;
+
+        // Display menu options
+        ggprint8b(&r, 16, 0x00ffffff, "Main Menu");
+        r.bot -= 40;
+        if (menuChoice == 0)
+            ggprint8b(&r, 16, 0x00ff0000, "> Start Game");
+        else
+            ggprint8b(&r, 16, 0x00ffffff, "Start Game");
+
+        r.bot -= 20;
+        if (menuChoice == 1)
+            ggprint8b(&r, 16, 0x00ff0000, "> Options");
+        else
+            ggprint8b(&r, 16, 0x00ffffff, "Options");
+
+        r.bot -= 20;
+        if (menuChoice == 2)
+            ggprint8b(&r, 16, 0x00ff0000, "> Quit");
+        else
+            ggprint8b(&r, 16, 0x00ffffff, "Quit");
+
+        glPopMatrix();
+        x11.swapBuffers();
+
+        // Handle menu input
+        XEvent e;
+        while (XCheckWindowEvent(x11.dpy, x11.win, KeyPressMask, &e)) {
+            if (e.type == KeyPress) {
+                if (e.xkey.keycode == 39) {
+                    // 'S' key to select menu option
+                    if (menuChoice == 0) {
+                        inMenu = false;  // Start the game
+                    } else if (menuChoice == 1) {
+                        // Open Options menu (customize this part)
+                        // Example: inOptions = true;
+                    } else if (menuChoice == 2) {
+                        // Quit the game
+                        exit(0);
+                    }
+                } else if (e.xkey.keycode == 111) {
+                    // Up arrow key
+                    menuChoice = (menuChoice + 2) % 3;
+                } else if (e.xkey.keycode == 116) {
+                    // Down arrow key
+                    menuChoice = (menuChoice + 1) % 3;
+                }
+            }
+        }
+    }
+}
 
 
 void nightmodefilter(int xres, int yres)
@@ -69,4 +131,4 @@ void display_name(int x, int y)
 	r.left = x;
 	r.center = 0;
     ggprint8b(&r, 0, 0xFFFFFFF, "Carlos");
-}
+}	
