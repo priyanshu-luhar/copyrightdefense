63476b7c3074f76f5c8c926e2d41345d8c0022e5
JosephHernandez1324 <143636261+JosephHernandez1324@users.noreply.github.com>
2023-10-23 09:30:21 -0700

    Add files via upload

---
+++ jhernandez2.h
@@ -0,0 +1,26 @@
+// Joseph Hernandez
+// CMPS 3350
+// Header File for Game Code
+
+#ifndef _JOSEPH_H_
+#define _JOSEPH_H_
+
+extern void check_keys();
+
+class Global {
+public: 
+    int mouse;
+    int xres, yres;
+    char keys[65536];
+    int show_name;
+    Global() {
+        xres = 640;
+        yres = 480;
+        memset(keys, 0, 65536);
+        show_name = 0;
+        mouse = 0;
+    }
+};
+extern class Global gl;
+
+#endif
