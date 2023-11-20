e50d38f34e0b8221b325d040d42063db2610a650
Priyanshu Luhar <69760063+priyanshu-luhar@users.noreply.github.com>
2023-11-20 06:41:00 -0800

    Latest Version with cleaned up individual headers

---
+++ pluhar.h
@@ -0,0 +1,19 @@
+#ifndef _PLUHAR_H_
+#define _PLUHAR_H_
+
+extern bool wasdvar;
+extern int render_calls;
+extern int physics_calls;
+extern bool wasdtoggle();
+extern int wasd(int key);
+//extern int total_running_time(bool temp);
+// extern int epx;
+// extern int py;
+extern double mouse_movement_distance(int x, int y, bool temp);
+extern int psavex;
+extern int psavey;
+
+// extern int total_render_function_calls(bool temp);
+// extern int total_physics_function_calls(bool temp);
+
+#endif
