da2842e8d03333ccf1fb9784f56f9ec962462963
Carlos Sandoval <113050074+CarlosSandoval360@users.noreply.github.com>
2023-12-04 23:12:18 -0800

    Update header.h
    Moved all the class declarations as well as the function declarations in each class to this file and moved all the constructors and de-constructors to main.cpp so as to not have any actual executable code in this header file that is supposed to be used universally with any addition .h and .cpp files

--- header.h
+++ header.h
@@ -1,14 +1,9 @@
 #ifndef _HEADER_H_
 #define _HEADER_H_
 
-/* Main Header File
- * Date: 19 Nov 2023
- * Priyanshu Luhar
- */
 #include <cstring>
 #include <iostream>
 #include <cstdlib>
-#include <cstring>
 #include <unistd.h>
 #include <ctime>
 #include <cmath>
@@ -18,168 +13,105 @@
 #include "log.h"
 #include "fonts.h"
 
+
+typedef float Flt;
+typedef float Vec[3];
+extern int Lives;
+extern bool gameWon;
+extern int countdown;
+extern time_t gameStartTime;
+extern const Flt MINIMUM_ASTEROID_SIZE;
+extern const float MINIMUM_ASTEROID_DISTANCE;
+inline float rnd() { return static_cast<float>(rand()) / static_cast<float>(RAND_MAX); }
+
+class Ship {
+public:
+    Vec pos;
+    Vec dir;
+    Vec vel;
+    Vec acc;
+    float angle;
+    float color[3];
+    float radius;
+    Ship();
+};
+
+class Bullet {
+public:
+    Vec pos;
+    Vec vel;
+    float color[6];
+    struct timespec time;
+    Bullet();
+};
+
+class Asteroid {
+public:
+    Vec pos;
+    Vec vel;
+    int nverts;
+    Flt radius;
+    Vec vert[8];
+    float angle;
+    float color[3];
+    Asteroid *prev;
+    Asteroid *next;
+    Asteroid();
+};
+
+class Game {
+public:
+    Ship ship;
+    Asteroid *ahead;
+    Bullet *barr;
+    int nasteroids;
+    int nbullets;
+    struct timespec bulletTimer;
+    struct timespec mouseThrustTimer;
+    bool mouseThrustOn;
+    Game();
+    ~Game();
+};
+
 class Global {
 public:
-	int xres, yres;
-	int show_toggle;
-	int nightmodefilter;
-	int renderMenu;
-	char keys[65536];
-	int mouse;
-	bool statistics;
-
-    // Don't touch.
-    // For Images, Priyanshu
+    int xres, yres;
+    int show_toggle;
+    int nightmodefilter;
+    int renderMenu;
+    char keys[65536];
+    int mouse;
+    bool statistics;
     GLuint menubackgroundTexture;
     GLuint mcTexture;
     int allowimages;
-	
-	Global() {
-		xres = 700;
-		yres = 540;
-		memset(keys, 0, 65536);
-		show_toggle = 0;
-		nightmodefilter = 0;
-		mouse = 0;
-		renderMenu = 0;
-		statistics = false;
-        allowimages = 1;
-	}
+    Global();
 };
 
 extern Global gl;
 
 class X11_wrapper {
 private:
-	GLXContext glc;
+    GLXContext glc;
 public:
-	Window win;
-	Display *dpy;
+    Window win;
+    Display *dpy;
 
-	X11_wrapper() { }
-	X11_wrapper(int w, int h) {
-		GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, GLX_DOUBLEBUFFER, None };
-		//GLint att[] = { GLX_RGBA, GLX_DEPTH_SIZE, 24, None };
-		XSetWindowAttributes swa;
-		setup_screen_res(gl.xres, gl.yres);
-		dpy = XOpenDisplay(NULL);
-		if (dpy == NULL) {
-			std::cout << "\n\tcannot connect to X server" << std::endl;
-			exit(EXIT_FAILURE);
-		}
-		Window root = DefaultRootWindow(dpy);
-		XWindowAttributes getWinAttr;
-		XGetWindowAttributes(dpy, root, &getWinAttr);
-		int fullscreen = 0;
-		gl.xres = w;
-		gl.yres = h;
-		if (!w && !h) {
-			//Go to fullscreen.
-			gl.xres = getWinAttr.width;
-			gl.yres = getWinAttr.height;
-			//When window is fullscreen, there is no client window
-			//so keystrokes are linked to the root window.
-			XGrabKeyboard(dpy, root, False,
-				GrabModeAsync, GrabModeAsync, CurrentTime);
-			fullscreen=1;
-		}
-		XVisualInfo *vi = glXChooseVisual(dpy, 0, att);
-		if (vi == NULL) {
-			std::cout << "\n\tno appropriate visual found\n" << std::endl;
-			exit(EXIT_FAILURE);
-		} 
-		Colormap cmap = XCreateColormap(dpy, root, vi->visual, AllocNone);
-		swa.colormap = cmap;
-		swa.event_mask = ExposureMask | KeyPressMask | KeyReleaseMask |
-			PointerMotionMask | MotionNotify | ButtonPress | ButtonRelease |
-			StructureNotifyMask | SubstructureNotifyMask;
-		unsigned int winops = CWBorderPixel|CWColormap|CWEventMask;
-		if (fullscreen) {
-			winops |= CWOverrideRedirect;
-			swa.override_redirect = True;
-		}
-		win = XCreateWindow(dpy, root, 0, 0, gl.xres, gl.yres, 0,
-			vi->depth, InputOutput, vi->visual, winops, &swa);
-		//win = XCreateWindow(dpy, root, 0, 0, gl.xres, gl.yres, 0,
-		//vi->depth, InputOutput, vi->visual, CWColormap | CWEventMask, &swa);
-		set_title();
-		glc = glXCreateContext(dpy, vi, NULL, GL_TRUE);
-		glXMakeCurrent(dpy, win, glc);
-		show_mouse_cursor(0);
-	}
-	~X11_wrapper() {
-		XDestroyWindow(dpy, win);
-		XCloseDisplay(dpy);
-	}
-	void set_title() {
-		//Set the window title bar.
-		XMapWindow(dpy, win);
-		XStoreName(dpy, win, "Copyright Onslaught!");
-	}
-	void check_resize(XEvent *e) {
-		//The ConfigureNotify is sent by the
-		//server if the window is resized.
-		if (e->type != ConfigureNotify)
-			return;
-		XConfigureEvent xce = e->xconfigure;
-		if (xce.width != gl.xres || xce.height != gl.yres) {
-			//Window size did change.
-			reshape_window(xce.width, xce.height);
-		}
-	}
-	void reshape_window(int width, int height) {
-		//window has been resized.
-		setup_screen_res(width, height);
-		glViewport(0, 0, (GLint)width, (GLint)height);
-		glMatrixMode(GL_PROJECTION); glLoadIdentity();
-		glMatrixMode(GL_MODELVIEW); glLoadIdentity();
-		glOrtho(0, gl.xres, 0, gl.yres, -1, 1);
-		set_title();
-	}
-	void setup_screen_res(const int w, const int h) {
-		gl.xres = w;
-		gl.yres = h;
-	}
-	void swapBuffers() {
-		glXSwapBuffers(dpy, win);
-	}
-	bool getXPending() {
-		return XPending(dpy);
-	}
-	XEvent getXNextEvent() {
-		XEvent e;
-		XNextEvent(dpy, &e);
-		return e;
-	}
-	void set_mouse_position(int x, int y) {
-		XWarpPointer(dpy, None, win, 0, 0, 0, 0, x, y);
-	}
-	void show_mouse_cursor(const int onoff) {
-		if (onoff) {
-			//this removes our own blank cursor.
-			XUndefineCursor(dpy, win);
-			return;
-		}
-		//vars to make blank cursor
-		Pixmap blank;
-		XColor dummy;
-		char data[1] = {0};
-		Cursor cursor;
-		//make a blank cursor
-		blank = XCreateBitmapFromData (dpy, win, data, 1, 1);
-		if (blank == None)
-			std::cout << "error: out of memory." << std::endl;
-		cursor = XCreatePixmapCursor(dpy, blank, blank, &dummy, &dummy, 0, 0);
-		XFreePixmap(dpy, blank);
-		//this makes the cursor. then set it using this function
-		XDefineCursor(dpy, win, cursor);
-		//after you do not need the cursor anymore use this function.
-		//it will undo the last change done by XDefineCursor
-		//(thus do only use ONCE XDefineCursor and then XUndefineCursor):
-	}
+    X11_wrapper();
+    X11_wrapper(int w, int h);
+    ~X11_wrapper();
+
+    void set_title();
+    void check_resize(XEvent *e);
+    void reshape_window(int width, int height);
+    void setup_screen_res(const int w, const int h);
+    void swapBuffers();
+    bool getXPending();
+    XEvent getXNextEvent();
+    void set_mouse_position(int x, int y);
+    void show_mouse_cursor(const int onoff);
 };
 
 extern X11_wrapper x11;
-
 
 #endif
