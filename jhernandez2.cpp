a954e6e53c143a64dff50c642ddba04db233af40
JosephHernandez1324 <143636261+JosephHernandez1324@users.noreply.github.com>
2023-12-05 23:23:51 -0800

    Update jhernandez2.cpp
    Added music code aka play_sound function

--- jhernandez2.cpp
+++ jhernandez2.cpp
@@ -12,6 +12,16 @@
 // Multiple animations implemented (Attack, hit, etc.)
 // Dynamic Health bar
 // Dynamic statuses that affect the player and enemies
+#ifdef USE_OPENAL_SOUND
+#include <stdio.h> //
+#include <string.h> //
+#include <math.h> //
+#include <unistd.h>
+#include <fcntl.h> //
+#include <sys/stat.h> //
+#include </usr/include/AL/alut.h>
+#endif //USE_OPENAL_SOUND
+
 #include <iostream>
 #include <unistd.h>
 #include <ctime>
@@ -57,4 +67,64 @@ int time_since_mouse_move(const bool get)
         return time(NULL) - start_time;
     }
     return 0;
-} 
+}
+
+void play_sound(int select)
+{
+#ifdef USE_OPENAL_SOUND
+    //Get started right here.
+    alutInit(0, NULL);
+    if (alGetError() != AL_NO_ERROR) {
+        printf("ERROR: alutInit()\n");
+        // return 0;
+    }
+    //Clear error state.
+    alGetError();
+    //
+    //Setup the listener.
+    //Forward and up vectors are used.
+    float vec[6] = {0.0f,0.0f,1.0f, 0.0f,1.0f,0.0f};
+    alListener3f(AL_POSITION, 0.0f, 0.0f, 0.0f);
+    alListenerfv(AL_ORIENTATION, vec);
+    alListenerf(AL_GAIN, 1.0f);
+    //
+    //Buffer holds the sound information.
+    ALuint alBuffer;
+    alBuffer = alutCreateBufferFromFile("./Background_Music.wav");
+    //
+    //Source refers to the sound.
+    ALuint alSource;
+    //Generate a source, and store it in a buffer.
+    alGenSources(1, &alSource);
+    alSourcei(alSource, AL_BUFFER, alBuffer);
+    //Set volume and pitch to normal, no looping of sound.
+    alSourcef(alSource, AL_GAIN, 1.0f);
+    alSourcef(alSource, AL_PITCH, 1.0f);
+    alSourcei(alSource, AL_LOOPING, AL_TRUE);
+    if (alGetError() != AL_NO_ERROR)
+        printf("ERROR: setting source\n");
+    // return 0;
+
+    if (select == 0)
+        alSourcePlay(alSource);
+
+    if (select == 1) {
+        //First delete the source.
+        alDeleteSources(1, &alSource);
+        //Delete the buffer.
+        alDeleteBuffers(1, &alBuffer);
+        //Close out OpenAL itself.
+        //Get active context.
+        ALCcontext *Context = alcGetCurrentContext();
+        //Get device for active context.
+        ALCdevice *Device = alcGetContextsDevice(Context);
+        //Disable context.
+        alcMakeContextCurrent(NULL);
+        //Release context(s).
+        alcDestroyContext(Context);
+        //Close device.
+        alcCloseDevice(Device);
+        // return 0;
+    }
+#endif
+}
