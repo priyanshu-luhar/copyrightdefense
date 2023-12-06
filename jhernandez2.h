ccf10dc36bc634c8ee714ac7cba1b369b904cdb7
JosephHernandez1324 <143636261+JosephHernandez1324@users.noreply.github.com>
2023-12-06 04:09:42 -0800

    Update jhernandez2.h

--- jhernandez2.h
+++ jhernandez2.h
@@ -10,30 +10,5 @@ extern int total_running_time(const bool);
 extern int time_since_mouse_move(const bool);
 extern void play_sound(int);
 extern void coins_buffs(int);
-/*
-class Global {
-    public: 
-        int mouse;
-        int xres, yres;
-        char keys[65536];
-        int show_name;
-        int statistics;
-        int show_toggle;
-        int nightmodefilter;
-        int rendermenu;
-        Global() {
-            xres = 640;
-            yres = 480;
-            memset(keys, 0, 65536);
-            show_name = 0;
-            show_toggle = 0;
-            nightmodefilter = 0;
-            mouse = 0;
-            statistics = 0;
-            rendermenu = 0;
-        }
-};
-extern class Global gl;
-*/
 
 #endif
