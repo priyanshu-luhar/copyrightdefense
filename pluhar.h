b2c779e18ffca172c5c1ca65e1408010ada4f338
Priyanshu Luhar <69760063+priyanshu-luhar@users.noreply.github.com>
2023-12-06 06:19:17 -0800

    Update pluhar.h

--- pluhar.h
+++ pluhar.h
@@ -6,14 +6,13 @@ extern int render_calls;
 extern int physics_calls;
 extern bool wasdtoggle();
 extern int wasd(int key);
-//extern int total_running_time(bool temp);
-// extern int epx;
-// extern int py;
 extern double mouse_movement_distance(int x, int y, bool temp);
 extern int psavex;
 extern int psavey;
-
-// extern int total_render_function_calls(bool temp);
-// extern int total_physics_function_calls(bool temp);
-
+/*
+extern bool buffbomb;
+extern void buffbomb();
+extern void checkbuffbomb(int count);
+extern void showbuffbomb();
+*/
 #endif
